fece35130e1854009e501a8f87c427853d670cad
moxuandi <1104984259@qq.com>
2019-12-11 19:55:04 +0800

    大小写

--- TagsinputAsset.php
+++ TagsinputAsset.php
@@ -9,7 +9,7 @@ use yii\web\AssetBundle;
  * @author zhangmoxuan <1104984259@qq.com>
  * @link http://www.zhangmoxuan.com
  * @QQ 1104984259
- * @Date 2019-8-20
+ * @Date 2019-8-21
  * @see https://github.com/bootstrap-tagsinput/bootstrap-tagsinput
  * @see http://bootstrap-tagsinput.github.io/bootstrap-tagsinput/examples/
  */
--- TagsinputWidget.php
+++ TagsinputWidget.php
@@ -11,7 +11,7 @@ use yii\widgets\InputWidget;
  * @author zhangmoxuan <1104984259@qq.com>
  * @link http://www.zhangmoxuan.com
  * @QQ 1104984259
- * @Date 2019-8-20
+ * @Date 2019-8-21
  * @see https://github.com/bootstrap-tagsinput/bootstrap-tagsinput
  * @see http://bootstrap-tagsinput.github.io/bootstrap-tagsinput/examples/
  */
